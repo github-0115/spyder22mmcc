7496cd37f126bd60c53269cec6ff7aef371fe5df
Hunterlisi <lisi-yang@live.com>
2016-07-25 20:58:45 +0800

    修改正则表示

---
+++ studyThreading.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+@version: ??
+@author: hunter
+@project: spyder22mmcc
+@license: Apache Licence 
+@file: studyThreading.py
+@time: 16-7-22 下午3:26
+"""
